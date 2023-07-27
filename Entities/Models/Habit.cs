f2d7d1edbdea195ae9dbe64a1c748c813c5222b2
brovig <brovig.f@gmail.com>
2023-07-27 20:31:35 +0300

    added models

---
+++ Entities/Models/Habit.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class Habit
+{
+    [Column("HabitId")]
+    public Guid Id { get; set; }
+
+    public bool IsArchived { get; set; }
+
+    public int Color { get; set; }
+
+    public string? Description { get; set; }
+
+    public int FrequencyDensity { get; set; }
+
+    public int FrequencyNumber { get; set; }
+
+    public int Highlight { get; set; }
+
+    [Required(ErrorMessage = "Name is a required field.")]
+    public string? Name { get; set; }
+
+    public int Position { get; set; }
+
+    public DateTime ReminderTime { get; set; }
+
+    public int ReminderDays { get; set; }
+
+    public bool IsMeasurable { get; set; }
+
+    public int Type { get; set; }
+
+    public int TargetType { get; set; }
+
+    public double TargetValue { get; set; }
+
+    public string? Unit { get; set; }
+
+    public string? Question { get; set; }
+}
---
+++ Entities/Models/Repetition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models;
+
+public class Repetition
+{
+    [Column("RepetitionId")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Timestamp is a required field.")]
+    public DateTime Timestamp { get; set; }
+
+    [Required(ErrorMessage = "Value is a required field.")]
+    public double Value { get; set; }
+
+    [ForeignKey(nameof(Habit))]
+    [Required(ErrorMessage = "HabitId is a required field.")]
+    public Guid HabitId { get; set; }
+}
