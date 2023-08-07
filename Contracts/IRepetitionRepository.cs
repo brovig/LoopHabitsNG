2e3014d09eb78ee1eca1dae0713e445e12e706d3
brovig <brovig.f@gmail.com>
2023-08-07 18:05:22 +0300

    repetition repository

--- Contracts/IRepetitionRepository.cs
+++ Contracts/IRepetitionRepository.cs
@@ -1,5 +1,11 @@
-﻿namespace Contracts;
+﻿using Entities.Models;
+
+namespace Contracts;
 
 public interface IRepetitionRepository
 {
+    Task<IEnumerable<Repetition>> GetAllRepetitionsAsync(Guid habitId, bool trackChanges);
+    Task<Repetition> GetRepetitionAsync(Guid habitId, int id, bool trackChanges);
+    void CreateRepetitionForHabit(Guid habitId, Repetition repetition);
+    void DeleteRepetition(Repetition repetition);
 }
--- Repository/RepetitionRepository.cs
+++ Repository/RepetitionRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository;
 
@@ -7,5 +8,26 @@ public class RepetitionRepository : RepositoryBase<Repetition>, IRepetitionRepos
 {
     public RepetitionRepository(RepositoryContext repositoryContext) : base(repositoryContext)
     {
+    }
+
+    public async Task<IEnumerable<Repetition>> GetAllRepetitionsAsync(Guid habitId, bool trackChanges)
+    {
+        return await FindByCondition(r => r.HabitId.Equals(habitId), trackChanges).ToListAsync();
+    }
+
+    public async Task<Repetition> GetRepetitionAsync(Guid habitId, int id, bool trackChanges)
+    {
+        return await FindByCondition(r => r.HabitId.Equals(habitId) && r.Id.Equals(id), trackChanges).SingleOrDefaultAsync();
+    }
+
+    public void CreateRepetitionForHabit(Guid habitId, Repetition repetition)
+    {
+        repetition.HabitId = habitId;
+        Create(repetition);
+    }
+
+    public void DeleteRepetition(Repetition repetition)
+    {
+        Delete(repetition);
     }
 }
