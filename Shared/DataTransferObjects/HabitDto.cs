c1d9f9d031944dfaee29b8156786a4819d81a21b
brovig <brovig.f@gmail.com>
2023-08-07 20:57:20 +0300

    data transfer objects added

---
+++ Shared/DataTransferObjects/HabitDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.DataTransferObjects;
+
+public record HabitDto(
+    Guid Id,
+    bool IsArchived,
+    int Color,
+    string Description,
+    int FrequencyDensity,
+    int FrequencyNumber,
+    int Highlight,
+    string Name,
+    int Position,
+    DateTime? ReminderTime,
+    int ReminderDays,
+    bool IsMeasurable,
+    int Type,
+    int TargetType,
+    int TargetValue,
+    string Unit,
+    string Question);
