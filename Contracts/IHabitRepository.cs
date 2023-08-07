6f4a9eab2dcd87b6e1e3f500f89bfca954db2516
brovig <brovig.f@gmail.com>
2023-08-07 17:19:54 +0300

    habit repository

--- Contracts/IHabitRepository.cs
+++ Contracts/IHabitRepository.cs
@@ -1,5 +1,12 @@
-﻿namespace Contracts;
+﻿using Entities.Models;
+
+namespace Contracts;
 
 public interface IHabitRepository
 {
+    Task<IEnumerable<Habit>> GetAllHabitsAsync(bool trackChanges);
+    Task<Habit> GetHabitAsync(Guid habitId, bool trackChanges);
+    Task<IEnumerable<Habit>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
+    void CreateHabit(Habit habit);
+    void DeleteHabit(Habit habit);
 }
--- Repository/HabitRepository.cs
+++ Repository/HabitRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Contracts;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository;
 
@@ -7,5 +8,31 @@ public class HabitRepository : RepositoryBase<Habit>, IHabitRepository
 {
     public HabitRepository(RepositoryContext repositoryContext) : base(repositoryContext)
     {
+    }
+
+
+    public async Task<IEnumerable<Habit>> GetAllHabitsAsync(bool trackChanges)
+    {
+        return await FindAll(trackChanges).OrderBy(p => p.Position).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Habit>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges)
+    {
+        return await FindByCondition(x => ids.Contains(x.Id), trackChanges).ToListAsync();
+    }
+
+    public async Task<Habit> GetHabitAsync(Guid habitId, bool trackChanges)
+    {
+        return await FindByCondition(h => h.Id.Equals(habitId), trackChanges).SingleOrDefaultAsync();
+    }
+
+    public void CreateHabit(Habit habit)
+    {
+        Create(habit);
+    }
+
+    public void DeleteHabit(Habit habit)
+    {
+        Delete(habit);
     }
 }
