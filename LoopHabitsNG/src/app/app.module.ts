890adb1c4f0a952b3de6c92ee7e8725fa15161c7
brovig <brovig.f@gmail.com>
2023-08-15 18:48:04 +0300

    replaced css with sass

--- LoopHabitsNG/src/app/app.module.ts
+++ LoopHabitsNG/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HabitsComponent } from './habits/habits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HabitsComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
