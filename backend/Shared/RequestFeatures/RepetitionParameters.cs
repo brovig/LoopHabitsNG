9e9c1b9e7a8375fd02cbc70f2fde09ed49b2773b
brovig <brovig.f@gmail.com>
2023-12-27 00:09:17 +0300

    fixed query dates verification in repetition parameters

--- backend/Shared/RequestFeatures/RepetitionParameters.cs
+++ backend/Shared/RequestFeatures/RepetitionParameters.cs
@@ -5,5 +5,5 @@ public class RepetitionParameters : RequestParameters
     public DateTime StartDate { get; set; } = DateTime.MinValue;
     public DateTime EndDate { get; set; } = DateTime.UtcNow;
 
-    public bool ValidDateRange => EndDate > StartDate;
+    public bool ValidDateRange => EndDate >= StartDate;
 }
